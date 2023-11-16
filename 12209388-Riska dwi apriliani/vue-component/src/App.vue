071e2a7288b4f7064937ec195dc47a98c35d9f24
riskadwi22 <riskadwiapriliani@smkwikrama.sch.id>
2023-11-16 15:02:29 +0700

    tugas component vue

---
+++ 12209388-Riska dwi apriliani/vue-component/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="row-service">
+    <Card v-for="item in data" :title="item.title" :description="item.description">{{ item.date }}
+    </Card>
+  </div>
+  <button class="btn-service">Detail</button>
+  <MyBtn id="btn1" name="btn"></MyBtn>
+  <MyBtn id="btn2" name="btn2"></MyBtn>
+</template>
+
+<script>
+import Card from '@/components/my-components/card.vue';
+export default{
+  components: {
+    Card,
+    'card-service':Card
+  },
+  data() {
+    return {
+      data:[
+        {
+          title: 'title 1',
+          description: 'description 1',
+          date: '20 Juni 2021'
+        },{
+          title: 'title ',
+          description: 'description 2',
+          date: '16 April 2023'
+        }
+      ]
+    }
+  },
+}
+</script>
+<style>
+.row-service{
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 10px;
+}
+.btn-service-global{
+   margin-top: 10px;
+    font-size: 16px;
+    font-weight: 400;
+    color: white;
+    background-color: #cf6262;
+    padding: 15px, 20px;
+    border: none;
+    border-radius: 5px;
+    transition: 0.6s;
+}
+</style>
---
+++ 12209388-Riska dwi apriliani/vue-component/src/components/my-components/card.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="card-service">
+        <slot></slot>
+        <h4>
+        {{ title }}
+        </h4>
+        <p>{{ description }}</p>
+        <button class="btn-service">Detail</button>
+        <MyBtn></MyBtn>
+    </div>
+</template>
+<script>
+export default{
+    props: ['title', 'description']
+}
+</script>
+<style scoped>
+
+.btn-service{
+    margin-top: 10px;
+    font-size: 16px;
+    font-weight: 400;
+    color: white;
+    background-color: #494d88;
+    padding: 15px, 20px;
+    border: none;
+    border-radius: 5px;
+    transition: 0.6s;
+}
+.card-service{
+    text-align: start;
+    min-height: 306px;
+    padding: 30px 30px 0 30px;
+    box-shadow: 0px 10px 30px rgba(201, 14, 14, 0.05);
+    border-radius: 60px;
+    background: white;
+    transition: all 0.5s ease;
+    margin: 10px, 10px;
+}
+.card-service h4{
+    font-size: 30px;
+    line-height: 30px;
+    color: #343b4e;
+}
+.card-service p{
+    font-weight: 400;
+    color: #827f8d;
+    margin: 0 0 10px 0;
+}
+.btn-service-global{
+    margin-top: 10px;
+    font-size: 20px;
+    font-weight: 400;
+    color: white;
+    background-color: #CE5A67;
+    padding: 15px, 20px;
+    border: none;
+    border-radius: 5px;
+}
+</style>
